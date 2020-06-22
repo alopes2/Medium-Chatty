b9a6aa53597b3ebda58fd15e1b8334ea66a3af45
Andre Lopes <andrelo@dunnhumby.com>
2020-06-22 17:51:41 +0200

    Use API to send request, instead of signalr

--- client/src/Chat/Chat.js
+++ client/src/Chat/Chat.js
@@ -5,37 +5,30 @@ import ChatWindow from './ChatWindow/ChatWindow';
 import ChatInput from './ChatInput/ChatInput';
 
 const Chat = () => {
-    const [ connection, setConnection ] = useState(null);
     const [ chat, setChat ] = useState([]);
     const latestChat = useRef(null);
 
     latestChat.current = chat;
 
     useEffect(() => {
-        const newConnection = new HubConnectionBuilder()
+        const connection = new HubConnectionBuilder()
             .withUrl('https://localhost:5001/hubs/chat')
             .withAutomaticReconnect()
             .build();
 
-        setConnection(newConnection);
-    }, []);
+        connection.start()
+            .then(result => {
+                console.log('Connected!');
 
-    useEffect(() => {
-        if (connection) {
-            connection.start()
-                .then(result => {
-                    console.log('Connected!');
-    
-                    connection.on('ReceiveMessage', message => {
-                        const updatedChat = [...latestChat.current];
-                        updatedChat.push(message);
-                    
-                        setChat(updatedChat);
-                    });
-                })
-                .catch(e => console.log('Connection failed: ', e));
-        }
-    }, [connection]);
+                connection.on('ReceiveMessage', message => {
+                    const updatedChat = [...latestChat.current];
+                    updatedChat.push(message);
+                
+                    setChat(updatedChat);
+                });
+            })
+            .catch(e => console.log('Connection failed: ', e));
+    }, []);
 
     const sendMessage = async (user, message) => {
         const chatMessage = {
@@ -43,16 +36,17 @@ const Chat = () => {
             message: message
         };
 
-        if (connection.connectionStarted) {
-            try {
-                await connection.send('SendMessage', chatMessage);
-            }
-            catch(e) {
-                console.log(e);
-            }
+        try {
+            await  fetch('https://localhost:5001/chat/messages', { 
+                method: 'POST', 
+                body: JSON.stringify(chatMessage),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
         }
-        else {
-            alert('No connection to server yet.');
+        catch(e) {
+            console.log('Sending message failed.', e);
         }
     }
 
