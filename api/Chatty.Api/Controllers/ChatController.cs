a2ada4977ab8492084f890fe924b760a04c4ddd3
Andre Lopes <andrelo@dunnhumby.com>
2020-06-22 16:45:55 +0200

    Add new ChatController with IHubContext

---
+++ api/Chatty.Api/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Chatty.Api.Hubs;
+using Chatty.Api.Hubs.Clients;
+using Chatty.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chatty.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
+
+        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        public async Task Post(ChatMessage message)
+        {
+            // run some logic...
+
+            await _chatHub.Clients.All.ReceiveMessage(message);
+        }
+    }
+}
